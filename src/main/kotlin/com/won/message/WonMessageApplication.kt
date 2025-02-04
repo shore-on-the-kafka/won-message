0ae76e7b73fe7515ec56009c8c2a1930da905238
lee wongi <wglee0731@naver.com>
2025-02-05 02:33:45 +0900

    Initial spring boot project

---
+++ src/main/kotlin/com/won/message/WonMessageApplication.kt
@@ -0,0 +1,11 @@
+package com.won.message
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WonMessageApplication
+
+fun main(args: Array<String>) {
+	runApplication<WonMessageApplication>(*args)
+}
